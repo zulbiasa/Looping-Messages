63bffd82726c209d37cd0be09d1e6531af8bf22b
USER <USER@ZULBIASA02>
2022-04-05 02:01:55 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intAnswer As Integer = DialogResult.No
+        Do
+            intAnswer = MessageBox.Show("Choose Yes to Exit, No to Continue", "Do Loop Example",
+MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+        Loop While (intAnswer = DialogResult.No)
+    End Sub
+End Class
